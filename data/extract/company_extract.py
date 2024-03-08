f314b535b51db36a012be35863b36d0c081bb484
c-stev <colejstevens@gmail.com>
2024-03-08 12:46:59 -0500

    Updated company_extract.py

--- data/extract/company_extract.py
+++ data/extract/company_extract.py
@@ -2,29 +2,26 @@ import csv
 import yfinance as yf
 
 
-def get_data(ticker_str, sector):
-    ticker = yf.Ticker(ticker_str)
-    data = {
-        'ticker': ticker_str.strip(),
-        'name': ticker.info.get('longName', '?').strip(),
-        'industry': ticker.info.get('industry', '?').strip(),
-        'sector': sector.strip(),
-        'country': ticker.info.get('country', '?').strip()
-    }
+# Makes use of yfinance and obtains the company country
+def get_data(symbol, name, sector):
+    ticker = yf.Ticker(symbol)
+    country = ticker.info.get('country', '?')
+    data = {'ticker': symbol.strip(), 'name': name.strip(), 'sector': sector.strip(), 'country': country.strip()}
     return data
 
 
 print("Pulling data from Yahoo! Finance...")
 
+# Writes the obtained data from the source file and yfinance to a new companies.csv file
 with open('../../data/companies.csv', mode='w', newline='', encoding='utf-8') as file:
-    writer = csv.DictWriter(file, fieldnames=['ticker', 'name', 'industry', 'sector', 'country'])
+    writer = csv.DictWriter(file, fieldnames=['ticker', 'name', 'sector', 'country'])
     writer.writeheader()
 
+    # Reads the original file and obtain the symbol and sector data
     with open('src/SP500.csv', mode='r', encoding='utf-8') as sp500:
         reader = csv.DictReader(sp500)
         for row in reader:
-            data = get_data(row['Symbol'], row['Sector'])
-            writer.writerow(data)
-
+            row_data = get_data(row['Symbol'], row['Name'], row['Sector'])
+            writer.writerow(row_data)
 
 print("File 'companies.csv' successfully created!")
