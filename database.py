e89c7708e5d8af63048f9078795446d7182b8fff
wbeau2 <61422839+wbeau2@users.noreply.github.com>
2024-03-24 13:44:31 -0400

    added the fact table to the database

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ import staging.stage_country as stage_country
 import staging.stage_company as stage_company
 import staging.stage_date as stage_date
 import staging.stage_financial_data as stage_fd
+import staging.stage_fact as stage_fact
 
 
 def create_database():
@@ -81,10 +82,12 @@ def create_database():
     #Create the fact Table for the stock analysis dimension
     cur.execute("""
         CREATE TABLE IF NOT EXISTS fact_stock_analysis(
-            date_key INTEGER REFERENCES dim_date(date_id),
-            company_key INTEGER REFERENCES dim_company(company_id),
-            country_key INTEGER REFERENCES dim_country(country_id),
-            financial_data_key INTEGER REFERENCES dim_financial(financial_data_id)
+            date_id INTEGER REFERENCES dim_date(date_id),
+            company_id INTEGER REFERENCES dim_company(company_id),
+            country_id INTEGER REFERENCES dim_country(country_id),
+            financial_data_id INTEGER REFERENCES dim_financial(financial_data_id),
+            returns FLOAT,
+            volatility FLOAT
         );
     """)
 
@@ -92,23 +95,26 @@ def create_database():
 
     #get all the data frames
     f_country = stage_country.get_staged_df()
-    f_country = f_country.rename(columns=lambda x: x.lower())
+    f_country.set_index('country_id', inplace=True)
 
     f_company = stage_company.get_staged_df()
-    f_company = f_company.rename(columns=lambda x: x.lower())
+    f_company.set_index('company_id', inplace=True)
 
     f_date = stage_date.get_staged_df()
-    f_date = f_date.rename(columns=lambda x: x.lower())
+    f_date.set_index('date_id', inplace=True)
 
     f_financial = stage_fd.get_staged_df()
-    f_financial = f_financial.rename(columns=lambda x: x.lower())
+    f_financial.set_index('financial_data_id', inplace=True)
 
+    f_fact = stage_fact.get_staged_df()
+    
 
     #append the data from all the data frames to their respected tables 
     f_country.to_sql("dim_country", con=engine, method='multi',if_exists='append', index=True, index_label="country_id",dtype=None)
     f_company.to_sql("dim_company", con=engine, method='multi',if_exists='append', index=True, index_label="company_id",dtype=None)
     f_date.to_sql("dim_date", con=engine, method='multi',if_exists='append', index=True, index_label="date_id",dtype=None)
     f_financial.to_sql("dim_financial", con=engine, method='multi',if_exists='append', index=True, index_label="financial_data_id",dtype=None)
+    f_fact.to_sql("fact_stock_analysis", con=engine, method='multi',if_exists='append', index=False, index_label=None,dtype=None)
 
     #close/dispose of all of the connections to the database 
     engine.dispose()
